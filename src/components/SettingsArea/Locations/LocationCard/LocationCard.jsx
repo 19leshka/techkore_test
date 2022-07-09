8179825faf89abef67ad6c7205297c63b824a9c3
Alexey Balakhanov <alexeybalahanov@gmail.com>
2022-07-09 12:29:06 +0300

    first commit

---
+++ src/components/SettingsArea/Locations/LocationCard/LocationCard.jsx
@@ -0,0 +1,9 @@
+import s from "./LocationCard.module.css";
+
+const LocationCard = ({id, region, defaultReg, holidays, leavePolicies, users}) => {
+  return (<div className={s.locationCard}>
+
+  </div>)
+}
+
+export default LocationCard;
